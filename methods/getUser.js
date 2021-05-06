abafd9a1a7b9f5ce70d10e2230c583b66a0b187d
Maxim <48844646+manaenckow@users.noreply.github.com>
2021-05-06 03:43:42 +0300

    Update getUser.js

--- methods/getUser.js
+++ methods/getUser.js
@@ -1,59 +1,60 @@
 let response;
-const getUser = (req, res, connection, api, user, ref) => {
-    connection.query('SELECT * FROM users WHERE vk_id = ?', [user], async (e, r) => {
-            if (!r[0]) {
-                const {first_name, last_name, photo_200} = (await api('users.get', {
-                    user_id: user,
-                    lang: 'ru',
-                    fields: 'photo_200'
-                }))[0];
+const getUser = async (req, res, pool, api, user, ref) => {
 
-                let ref_name, ref_vk_id, ref_photo_200 = '';
+    const connection = pool.promise();
 
-                if (parseInt(ref)) {
-                    const refUserData = (await api('users.get', {
-                        user_id: parseInt(ref),
-                        lang: 'ru',
-                        fields: 'photo_200',
-                        name_case: 'acc'
-                    }))[0];
+    const [user_data] = await connection.query('SELECT * FROM users WHERE vk_id = ?', [user]);
 
-                    ref_name = `${refUserData.first_name} ${refUserData.last_name}`;
-                    ref_vk_id = refUserData.id;
-                    ref_photo_200 = refUserData.photo_200;
-                }
+    if (!user_data[0]) {
+        const {first_name, last_name, photo_200} = (await api('users.get', {
+            user_id: user,
+            lang: 'ru',
+            fields: 'photo_200'
+        }))[0];
 
-                connection.query(
-                    'INSERT INTO users (vk_id, name, photo_200, owner_name, owner_vk_id, owner_photo, is_infected) ' +
-                    'VALUES (?, ?, ?, ?, ?, ?, ?)',
-                    [
-                        user,
-                        `${first_name} ${last_name}`,
-                        photo_200,
-                        ref_name,
-                        ref_vk_id || 0,
-                        ref_photo_200,
-                        ref_vk_id ? 1 : 0
-                    ], (e, r) => {
-                        if (e) return e;
-                        response = {
-                            coins: 10,
-                            is_infected: ref_vk_id ? 1 : 0,
-                            virus_instance: 1,
-                            owner_name: ref_name,
-                            owner_vk_id: ref_vk_id || 0,
-                            owner_photo_200: ref_photo_200,
-                            vk_id: user,
-                            heals: 0,
-                            vip: 0
-                        };
-                    });
-            } else {
-                response = r[0];
-            }
+        let ref_name, ref_vk_id, ref_photo_200 = '';
+
+        if (parseInt(ref)) {
+            const refUserData = (await api('users.get', {
+                user_id: parseInt(ref),
+                lang: 'ru',
+                fields: 'photo_200',
+                name_case: 'acc'
+            }))[0];
+
+            ref_name = `${refUserData.first_name} ${refUserData.last_name}`;
+            ref_vk_id = refUserData.id;
+            ref_photo_200 = refUserData.photo_200;
         }
-    );
-    return response;
+
+        connection.query(
+            'INSERT INTO users (vk_id, name, photo_200, owner_name, owner_vk_id, owner_photo, is_infected) ' +
+            'VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [
+                user,
+                `${first_name} ${last_name}`,
+                photo_200,
+                ref_name,
+                ref_vk_id || 0,
+                ref_photo_200,
+                ref_vk_id ? 1 : 0
+            ], (e, r) => {
+                if (e) return e;
+                response = {
+                    coins: 10,
+                    is_infected: ref_vk_id ? 1 : 0,
+                    virus_instance: 1,
+                    owner_name: ref_name,
+                    owner_vk_id: ref_vk_id || 0,
+                    owner_photo_200: ref_photo_200,
+                    vk_id: user,
+                    heals: 0,
+                    vip: 0
+                };
+            });
+    } else {
+       return user_data[0];
+    }
 }
 
 module.exports = getUser;
